32e4a3366513f92b78ddc5a458d54fbd317d9ed3
inxait <administracionit@inxaitcorp.com>
2022-09-21 10:39:35 -0500

    config pluggins, livewire and start migrations

--- resources/views/layouts/theme/scripts.blade.php
+++ resources/views/layouts/theme/scripts.blade.php
@@ -1,4 +1,4 @@
-<!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
+<!-- START SCRIPTS -->
 <script src="{{ asset('assets/js/libs/jquery-3.1.1.min.js') }}"></script>
 <script src="{{ asset('bootstrap/js/popper.min.js') }}"></script>
 <script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
@@ -10,9 +10,19 @@
     });
 </script>
 <script src="{{ asset('assets/js/custom.js') }}"></script>
-<!-- END GLOBAL MANDATORY SCRIPTS -->
-
-<!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
-<script src="{{ asset('plugins/apex/apexcharts.min.js') }}"></script>
-<script src="{{ asset('assets/js/dashboard/dash_2.js') }}"></script>
-<!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
+<script src="{{ asset('plugins/sweetalerts/sweetalert2.min.js') }}"></script>
+<script src="{{ asset('plugins/notification/snackbar/snackbar.min.js') }}"></script>
+<script src="{{ asset('plugins/nicescroll/nicescroll.js') }}"></script>
+<script src="{{ asset('plugins/currency/currency.js') }}"></script>
+<script>
+    function noty(msg, option = 1) {
+        Snackbar.show({
+            text: msg.toUpperCase(),
+            actionText: 'CERRAR',
+            actionTextColor: '#fff',
+            backgroundColor: option == 1 ? '#3b3f5c' : '#e7515a',
+            pos: 'top-right'
+        });
+    }
+</script>
+<!-- END SCRIPTS -->
